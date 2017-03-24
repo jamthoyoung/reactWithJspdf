b5b98e6427d6362c7af89169d8ba59be240e003b
James Young <jamthoyoung@gmail.com>
2017-03-24 15:05:24 -0400

    Add code to show image in pdf.

--- src/App.js
+++ src/App.js
@@ -7,18 +7,41 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
+    this.toDataUrl = this.toDataUrl.bind(this);
+  }
+
+  toDataUrl(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        callback(reader.result);
+      }
+      reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
   }
 
   onClick() {
       var doc = new pdfConverter('p','pt','c6');
-      doc.setFontSize(22);
-      doc.text(20, 50, 'Park Entry Ticket');
-      doc.setFontSize(16);
-      doc.text(20, 80, 'Address1: ' );
-      doc.text(20, 100, 'Address2: ' );
-      doc.text(20, 120, 'Entry Date & time: ');
-      doc.text(20, 140, 'Expiry date & time: ' );
-      doc.save("test.pdf");
+      this.toDataUrl('background.jpg', function(base64Img) {
+        var imgData = base64Img;
+        console.log(imgData);
+        console.log('Adding to doc.');
+        doc.addImage(imgData, 'JPEG', 15, 40, 180, 160);
+        console.log('Image added.');
+        doc.setFontSize(22);
+        doc.text(20, 50, 'Park Entry Ticket');
+        doc.setFontSize(16);
+        doc.text(20, 80, 'Address1: ' );
+        doc.text(20, 100, 'Address2: ' );
+        doc.text(20, 120, 'Entry Date & time: ');
+        doc.text(20, 140, 'Expiry date & time: ' );
+        console.log('About to save');
+        doc.save("test.pdf");
+      });
   }
 
   render() {
