5219f9f9d121e4080f160198235296b6874e0b65
James Young <jamthoyoung@gmail.com>
2017-03-24 12:41:38 -0400

    Works with jspdf.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import pdfConverter from 'jspdf';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+      var doc = new pdfConverter('p','pt','c6');
+      doc.setFontSize(22);
+      doc.text(20, 50, 'Park Entry Ticket');
+      doc.setFontSize(16);
+      doc.text(20, 80, 'Address1: ' );
+      doc.text(20, 100, 'Address2: ' );
+      doc.text(20, 120, 'Entry Date & time: ');
+      doc.text(20, 140, 'Expiry date & time: ' );
+      doc.save("test.pdf");
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +30,8 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <input type='button'
+        onClick={this.onClick} value="Print"/>
         </p>
       </div>
     );
